85d46968ace6ad1bce42f866bf23777ea56eba26
liuxin <liuxin@fscut.com>
2025-06-24 17:25:26 +0800

    尝试构建ifcOpenShell，但遇到的bug越来越多了。

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools import cmake
+from conan.tools.cmake import CMakeToolchain
+
+class IFCOpenShellRecipe(ConanFile):
+    name = "ifcopenshell"
+    version = "0.8.2"
+
+    settings = "os", "arch"
+
+    def requirements(self):
+        self.requires("opencascade/7.8.0.patch.1@third_party/stable")
+        self.requires("boost/1.85.0@third_party/stable")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = None  # prevent CMakeUserPresets.json from being generated
+        tc.generate()
+
+    def layout(self):
+        self.folders.build = "build"  # 所有构建都在 build/ 下
+        self.folders.generators = "build/generators"
