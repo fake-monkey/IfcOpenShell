266bb359cfc4a3ec30377b86cf55c33674b416d7
liuxin <liuxin@fscut.com>
2025-06-25 10:36:56 +0800

    终于cmake成功了。

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,8 @@
 from conan import ConanFile
 from conan.tools import cmake
 from conan.tools.cmake import CMakeToolchain
+from os import path
+from pathlib import Path
 
 class IFCOpenShellRecipe(ConanFile):
     name = "ifcopenshell"
@@ -15,4 +17,9 @@ class IFCOpenShellRecipe(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.user_presets_path = None  # prevent CMakeUserPresets.json from being generated
+        for k, v in self.dependencies._data.items():
+            pkg = str(v)[:str(v).find('/')]
+            pkg_name = str(self.dependencies[pkg].description)
+            tc.variables[pkg_name + "_DIR"] = Path(
+                path.join(self.dependencies[pkg].package_folder, "cmake")).as_posix()
         tc.generate()
