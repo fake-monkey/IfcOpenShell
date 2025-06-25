3c621dbee2a676b02ba08688378425d505b28e8b
liuxin <liuxin@fscut.com>
2025-06-25 13:12:33 +0800

    勉强打通拉取包，使用包的过程，但还是出现问题。

--- conanfile.py
+++ conanfile.py
@@ -13,6 +13,7 @@ class IFCOpenShellRecipe(ConanFile):
     def requirements(self):
         self.requires("opencascade/7.8.0.patch.1@third_party/stable")
         self.requires("boost/1.85.0@third_party/stable")
+        self.requires("eigen/3.4.0@third_party/stable")
 
     def generate(self):
         tc = CMakeToolchain(self)
