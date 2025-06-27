8b3d7793de38e73be5abd3e5b25691b7dbdaf59a
liuxin <liuxin@fscut.com>
2025-06-27 19:49:12 +0800

    统一改为用target描述。

--- cmake/ifc_open_shell_targets.cmake
+++ cmake/ifc_open_shell_targets.cmake
@@ -9,9 +9,27 @@ function (link_opencascade TARGET_NAME SCOPE)
         # @todo investigate the exact conditions when this is necessary
         TKBin
     )
+    
+    # 下面流程摘抄自 cmake/CMakeLists.txt
+
+    if(OCC_INCLUDE_DIR)
+        file(STRINGS ${OCC_INCLUDE_DIR}/Standard_Version.hxx OCC_MAJOR
+            REGEX "#define OCC_VERSION_MAJOR.*"
+        )
+        string(REGEX MATCH "[0-9]+" OCC_MAJOR ${OCC_MAJOR})
+        file(STRINGS ${OCC_INCLUDE_DIR}/Standard_Version.hxx OCC_MINOR
+          REGEX "#define OCC_VERSION_MINOR.*"
+        )
+        string(REGEX MATCH "[0-9]+" OCC_MINOR ${OCC_MINOR})
+        file(STRINGS ${OCC_INCLUDE_DIR}/Standard_Version.hxx OCC_MAINT
+          REGEX "#define OCC_VERSION_MAINTENANCE.*"
+        )
+        string(REGEX MATCH "[0-9]+" OCC_MAINT ${OCC_MAINT})
+        set(OCC_VERSION_STRING "${OCC_MAJOR}.${OCC_MINOR}.${OCC_MAINT}")
+    endif(OCC_INCLUDE_DIR)
 
     if(OCC_VERSION_STRING VERSION_LESS 7.8.0)
-        # 公司构建的 open cascade 中没有 TKIGES TKSTEPBase TKSTEPAttr TKSTEP209 TKSTEP
+        list(APPEND OPENCASCADE_LIBRARY_NAMES  TKIGES TKSTEPBase TKSTEPAttr TKSTEP209 TKSTEP)
     else(OCC_VERSION_STRING VERSION_LESS 7.8.0)
         list(APPEND OPENCASCADE_LIBRARY_NAMES TKDESTEP TKDEIGES)
     endif(OCC_VERSION_STRING VERSION_LESS 7.8.0)
