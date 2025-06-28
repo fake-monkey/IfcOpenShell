bfbfb507bc3efaa016c0f845f8731707fe342ad2
liuxin <liuxin@fscut.com>
2025-06-28 16:27:23 +0800

    漏了导出测试。

--- conan_test/conanfile_test_export.bat
+++ conan_test/conanfile_test_export.bat
@@ -8,6 +8,7 @@ cmake --install build\Debug --config Debug
 cmake . --preset VS2022_x64-Release
 cmake --build --preset Release -j %NUMBER_OF_PROCESSORS%
 cmake --install build\Release --config Release
+conan export-pkg . -of build\conan_export
 
 endlocal
 pause
