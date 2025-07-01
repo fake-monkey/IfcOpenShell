fd2a5bfe627b8e0411180670cc600487dc039c73
liuxin <liuxin@fscut.com>
2025-07-01 19:47:52 +0800

    同步bat脚本的修改。

--- conan_test/conanfile_test_export.bat
+++ conan_test/conanfile_test_export.bat
@@ -2,13 +2,14 @@ setlocal
 
 cd /d %~dp0..\
 rmdir /s /q build\package
-call %~dp0conan_configure.bat Debug
+call %~dp0cmake_configure.bat Debug
 cmake --build --preset Debug -j %NUMBER_OF_PROCESSORS%
 cmake --install build\Debug --config Debug
-call %~dp0conan_configure.bat Release
+call %~dp0cmake_configure.bat Release
 cmake --build --preset Release -j %NUMBER_OF_PROCESSORS%
 cmake --install build\Release --config Release
 pushd build\package\
+del /q ..\ifcopenshell.zip
 7z a ..\ifcopenshell.zip *
 popd
 copy conanfile.py build\package\ /Y
