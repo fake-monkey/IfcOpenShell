40810970a12e851c496a85263182c39c9f60ba57
liuxin <liuxin@fscut.com>
2025-07-01 15:10:30 +0800

    写错了配置用的脚本。

--- conan_test/conanfile_test_export.bat
+++ conan_test/conanfile_test_export.bat
@@ -2,10 +2,10 @@ setlocal
 
 cd /d %~dp0..\
 rmdir /s /q build\package
-call %~dp0conan_configure.bat Debug
+call %~dp0cmake_configure.bat Debug
 cmake --build --preset Debug -j %NUMBER_OF_PROCESSORS%
 cmake --install build\Debug --config Debug
-call %~dp0conan_configure.bat Release
+call %~dp0cmake_configure.bat Release
 cmake --build --preset Release -j %NUMBER_OF_PROCESSORS%
 cmake --install build\Release --config Release
 pushd build\package\
